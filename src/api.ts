89e4192684ff19f3e60fddbd50b423df45af948d
Ozpy <oscarale46@gmail.com>
2023-06-02 11:53:34 -0700

    feat: upload

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,59 @@
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
+import AWS from 'aws-sdk';
 
 export const app = express();
 
 app.use(cors({ origin: true }));
-
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
-// Healthcheck endpoint
+// Configuración de AWS
+const s3 = new AWS.S3({
+  accessKeyId: process.env.DO_SPACES_ID,
+  secretAccessKey: process.env.DO_SPACES_SECRET,
+  endpoint: new AWS.Endpoint(process.env.DO_SPACE_URL),
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('media'), (req:any, res) => {
+  const file = req.file;
+
+  console.log('ENV:', process.env)
+
+  if (!file) {
+    res.status(400).json({ error: 'No se ha proporcionado ningún archivo' });
+    return;
+  }
+
+  const bucketName = process.env.DO_SPACES_BUCKET;
+  const params = {
+    Bucket: bucketName,
+    Key:Date.now()+'_'+ file.originalname,
+    Body: file.buffer,
+    ACL: 'public-read',
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error('Error al cargar el archivo:', err);
+      res.status(500).json({ error: 'Error al cargar el archivo' });
+    } else {
+      const imageUrl = data.Location;
+      console.log('Archivo cargado exitosamente:', imageUrl);
+      res.json({ imageUrl });
+    }
+  });
+});
+
 app.get('/', (req, res) => {
   res.status(200).send({ status: 'ok' });
 });
 
-const api = express.Router();
-
-api.get('/hello', (req, res) => {
-  res.status(200).send({ message: 'hello world' });
+app.get('/api/v1/hello', (req, res) => {
+  res.status(200).send({ message: 'Hello world' });
 });
-
-// Version the api
-app.use('/api/v1', api);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { config } from 'dotenv';
 
+
 if (process.env.NODE_ENV !== 'production') {
+  console.log('Loading dev environments');
   config();
 }
+
 // call after config() to access the env variables
 import { app } from './api';
 
+console.log('hola::'+process.env.DO_SPACE_URL)
 const port = process.env.PORT || 3333;
 
 app.listen(port, () =>
