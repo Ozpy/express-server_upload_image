264f96d0f3bb0d69129c02122d2b6eff569cf49f
Ozpy <oscarale46@gmail.com>
2023-08-11 02:17:19 -0600

    Clean code

--- src/api.ts
+++ src/api.ts
@@ -26,8 +26,6 @@ const upload = multer({ storage });
 app.post('/upload', upload.single('media'), (req: any, res) => {
   const file = req.file;
 
-  console.log('ENV:', process.env);
-
   if (!file) {
     res.status(400).json({ error: 'No se ha proporcionado ningún archivo' });
     return;
@@ -78,39 +76,27 @@ async function createCollection(nameCollection: any) {
 async function addDocument({ collectionName, document }) {
   const collection = await client.getCollection(collectionName);
   const doc = await collection.add(document);
-  console.log('🚀 ~ file: api.ts:79 ~ addDocument ~ doc:', doc);
   return doc;
 }
 
 async function queryCollection(collectionName: any, query: any) {
   // const collection = await client.getCollection(collectionName)
   // const results = await collection.query(query)
-  // console.log("🚀 ~ file: api.ts:79 ~ addDocument ~ doc:", results)
   // return results;
 }
 
 app.post('/api/v1/createEmbedding', (req, res) => {
-  console.log(
-    '🚀 ~ file: api.ts:93 ~ app.post ~ req:',
-    req.body.nameCollection
-  );
-  // console.log("🚀 ~ file: api.ts:94 ~ app.post ~ req:", req.body)
-  // console.log("🚀 ~ file: api.ts:94 ~ app.post ~ req:", req)
   createCollection(req.body.nameCollection)
     .then((collection: any) => {
-      console.log('🚀 ~ file: api.ts:96 ~ .then ~ collection:', collection);
       addDocument({
         collectionName: req.body.nameCollection,
         document: req.body.document
       });
     })
     .then((doc: any) => {
-      console.log('🚀 ~ file: api.ts:96 ~ .then ~ doc:', doc);
       return queryCollection(req.body.nameCollection, req.body.query);
     })
     .then((results: any) => {
-      // console.log ("🚀 ~ file: api.ts:96 ~ .then ~ results:", results)
-      console.log('create_chat_embedding');
       res
         .status(200)
         .send({ message: 'create_chat_embedding', results: results });
@@ -119,16 +105,11 @@ app.post('/api/v1/createEmbedding', (req, res) => {
       console.log('errorcreate_chat_embedding');
       console.log(err);
     });
-  console.log('create_chat_embedding');
 
   res.status(200).send({ message: 'create_chat_embedding' });
 });
 
 app.post('/api/v1/bye', (req, res) => {
-  // console.log("🚀 ~ file: api.ts:126 ~ app.post ~ req:", req)
-  // console.log('bye')
-  // console.log(res)
-
   res.status(200).send({ message: 'Hello world' });
 });
 
@@ -153,7 +134,6 @@ app.post('/api/v1/gptExecuteSQL', (req, res) => {
   const prompt = 'conoces a alguna programadora';
 
   queryData('langchain', prompt).then((result: any) => {
-    console.log('🚀 ~ file: api.ts:3234 ~ ).then ~ result:', result);
     res.status(200).send({ message: 'Hello world', result: result });
   });
 });
@@ -164,15 +144,12 @@ app.post('/api/v1/gptUpsertData', (req, res) => {
 
   formatData(data)
     .then((res: any) => {
-      console.log('🚀 ~ file: api.ts:172 ~ formatData ~ res', res);
       return upsertData(indexName, res);
     })
     .then((result: any) => {
-      console.log('🚀 ~ file: api.ts:173 ~ formatData ~ result:', result);
       res.status(200).send({ message: 'Search', result: result });
     })
     .catch((err: any) => {
-      console.log('🚀 ~ file: api.ts:174 ~ formatData ~ err:', err);
       res.status(500).send({ message: 'Error', result: err });
     });
 });
@@ -183,11 +160,9 @@ app.post('/api/v1/gptSearch', (req, res) => {
 
   queryData(indexName, query)
     .then((result: any) => {
-      console.log('🚀 ~ file: api.ts:173 ~ formatData ~ result:', result);
       res.status(200).send({ message: 'Search', result: result });
     })
     .catch((err: any) => {
-      console.log('🚀 ~ file: api.ts:174 ~ formatData ~ err:', err);
       res.status(500).send({ message: 'Error', result: err });
     });
 });
@@ -196,7 +171,6 @@ import { PineconeClient } from '@pinecone-database/pinecone';
 
 async function formatData(data: any) {
   return await processVectors(data).then((result: any) => {
-    console.log('🚀 ~ file: api.ts:172 ~ formatData ~ result:', result);
     return result;
   });
 }
@@ -228,13 +202,10 @@ async function initializePinecone() {
 // async function getIndexes() {
 //   const pinecone = await initializePinecone();
 //   const indexesList = await pinecone.listIndexes();
-//   console.log('🚀 ~ file: api.ts:159 ~ getIndexes ~ indexesList:', indexesList);
-
 //   return indexesList;
 // }
 
 async function upsertData(indexName: any, data: any) {
-  console.log('🚀 ~ file: api.ts:222 ~ upsertData ~ data:', data);
   const pinecone = await initializePinecone();
   const index = pinecone.Index(indexName);
 
@@ -243,9 +214,6 @@ async function upsertData(indexName: any, data: any) {
       vectors: data
     }
   });
-
-  console.log('🚀 ~ file: api.ts:159 ~ getIndexes ~ indexesList:', result);
-
   return result;
 }
 
@@ -253,8 +221,6 @@ async function queryData(indexName: any, text: any) {
   // const indexes = getIndexes();
 
   const embed = await embedText(text);
-
-  console.log('🚀 ~ file: api.ts:833 ~ queryData ~ embed:', embed);
   const pinecone = await initializePinecone();
   const index = pinecone.Index(indexName);
 
@@ -265,9 +231,6 @@ async function queryData(indexName: any, text: any) {
       topK: 5
     }
   });
-
-  console.log('🚀 ~ file: api.ts:159 ~ getIndexes ~ indexesList:', result);
-
   return result;
 }
 
@@ -294,11 +257,6 @@ export async function embedText(text: string) {
 }
 
 // export async function createCompletion({ messages, temperature = 1 }) {
-//   console.log(
-//     '🚀 ~ file: openai.js:20 ~ createCompletion ~ messages:',
-//     messages
-//   );
-
 //   const systemMessages = messages.filter(
 //     (message) => message.role === openaiRoles.SYSTEM
 //   );
@@ -306,26 +264,13 @@ export async function embedText(text: string) {
 //   const noSystemMessages = messages.filter(
 //     (message) => message.role !== openaiRoles.SYSTEM
 //   );
-
-//   console.log(
-//     '🚀 ~ file: openai.js:31 ~ createCompletion ~ noSystemMessages:',
-//     noSystemMessages
-//   );
 //   const lastMessages =
 //     noSystemMessages.length > 10
 //       ? noSystemMessages.slice(-10)
 //       : noSystemMessages;
-//   console.log(
-//     '🚀 ~ file: openai.js:36 ~ createCompletion ~ lastMessages:',
-//     lastMessages
-//   );
-
 //   const prompt = lastMessages
 //     .slice(0, lastMessages.length - 1)
 //     .concat(systemMessages, lastMessages.slice(lastMessages.length - 1));
-
-//   console.log('🚀 ~ file: openai.js:33 ~ createCompletion ~ prompt:', prompt);
-
 //   try {
 //     const completion = await openai.createChatCompletion({
 //       temperature,
