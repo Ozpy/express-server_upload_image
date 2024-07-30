40774828a89162a56e874e0bc792d073345d17ae
Ozpy <oscarale46@gmail.com>
2024-07-30 17:01:01 -0600

    testing 3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { config } from 'dotenv';
 
-
 if (process.env.NODE_ENV !== 'production') {
   console.log('Loading dev environments');
   config();
@@ -9,9 +8,9 @@ if (process.env.NODE_ENV !== 'production') {
 // call after config() to access the env variables
 import { app } from './api';
 
-console.log('hola::'+process.env.DO_SPACE_URL)
+console.log('hola::' + process.env.DO_SPACE_URL);
 const port = process.env.PORT || 3333;
 
 app.listen(port, () =>
-  console.log(`API available on http://localhost:${port}`)
+  console.log(`API  available on http://localhost:${port}`)
 );
